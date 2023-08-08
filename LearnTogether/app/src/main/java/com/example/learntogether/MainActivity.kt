6c9d64450de9cc2d994f36ecbf4b8db43fcbaf7a
Joseph Garcia <Joseph_Garcia@ucne.edu.do>
2023-08-08 12:29:12 -0400

    Finished LearnTogether

--- LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
+++ LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage()
                 }
             }
         }
@@ -30,20 +38,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun GreetingText(Text1: String,Text2: String,Text3: String,modifier: Modifier = Modifier){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = Text1,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp)
+    )
+    Text(
+        text = Text2,
+        fontSize = 14.sp,
+        modifier = Modifier.padding(16.dp),
+        textAlign = TextAlign.Justify
+    )
+    Text(
+        text = Text3,
+        fontSize = 14.sp,
+        modifier = Modifier.padding(16.dp),
+        textAlign = TextAlign.Justify
     )
 }
 
+
 @Preview(
     showBackground = true,
-    
+    name = "LearnTogether",
+    showSystemUi = true
     )
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        Column {
+            GreetingImage()
+            GreetingText(
+                Text1 = "Jetpack Compose tutorial",
+                Text2 = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                Text3 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+            )
+        }
     }
 }
